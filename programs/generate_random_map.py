0308a48ab3c5448e3250b0bbf957e0d766ab42ca
Teiolass <alessio.marchetti22@gmail.com>
2018-05-15 19:40:18 +0200

    Migliorato il salesman. Aggiunto gitignore per ignorare file csv di utility

---
+++ programs/generate_random_map.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+inp = input('Numero citta: ')
+n_cities = int(inp)
+
+
+
+map = np.random.rand(n_cities, 2)
+
+txt = ''
+for city in map:
+    txt += str(city[0])
+    txt += ','
+    txt += str(city[1])
+    txt += '\n'
+
+with open('map.csv', 'w') as file:
+    file.write(txt)
+
+print('File scritto con successo!')
